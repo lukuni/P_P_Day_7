1c40416aa0146930a4de77963fd3818b4b4e92ab
Oyumaa Chimeddagva <feelingjasmine@gmail.com>
2025-06-10 23:29:34 +0800

    Day 8 extension: Added detailed logging to StockMove class

--- src/StockMove.java
+++ src/StockMove.java
@@ -1,31 +1,27 @@
-/**
- * Барааг нэг байршлаас нөгөө байршил руу шилжүүлэх үйлдлийг гүйцэтгэх класс.
- */
-public class StockMove {
-    /** Барааг шилжүүлэх эхний байршил */
-    Location from;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-    /** Бараа очих байршил */
+public class StockMove {
+    private static final Logger logger = LogManager.getLogger(StockMove.class);
+
+    Location from;
     Location to;
 
-    /**
-     * Шилжүүлэлтийн объектыг үүсгэнэ.
-     * 
-     * @param from Барааг шилжүүлэх эхний байршил
-     * @param to   Бараа очих байршил
-     */
     public StockMove(Location from, Location to) {
         this.from = from;
         this.to = to;
+        logger.debug("StockMove объект үүссэн: from={} to={}", from.getName(), to.getName());
     }
 
-    /**
-     * Тодорхой барааг эхний байршлаас хасч, хоёр дахь байршилд нэмнэ.
-     * 
-     * @param p Шилжүүлэх бараа
-     */
     public void moveProduct(Product p) {
-        from.removeProduct(p);
-        to.addProduct(p);
+        logger.info("Бараа шилжүүлж байна: {} -с {} рүү", p.getName(), from.getName(), to.getName());
+        try {
+            from.removeProduct(p);
+            to.addProduct(p);
+            logger.info("Бараа амжилттай шилжлээ: {}", p.getName());
+        } catch (IllegalArgumentException e) {
+            logger.error("Алдаа гарлаа барааг шилжүүлэхэд: {}", e.getMessage());
+            throw e; // дахин шидэх боломжтой
+        }
     }
 }
